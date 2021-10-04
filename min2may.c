cfd1ab83661db35868ea593f2a54ba0bcb6cb008
Gael <gaelrodguez@hotmail.com>
2021-10-04 13:06:25 +0000

    add min2may

---
+++ min2may.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+    char c, upperC;
+    read(STDIN_FILENO, &c, 1);
+    upperC = toupper(c);
+    write(STDOUT_FILENO, &upperC, 1);
+    
+    return 0;
+}
